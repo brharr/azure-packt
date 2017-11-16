e6fe6a2765d1c8d31d3aa8720f2e6ee80de776e9
Brian C Harrison <briancharrison@Brians-MBP.fios-router.home>
2017-11-15 22:29:50 -0500

    Updates to Section 4

--- iaas/section4cli.sh
+++ iaas/section4cli.sh
@@ -4,3 +4,12 @@ location = "eastus"
 vnet = "vnet-packt"
 bastionip = "BastionIP"
 bastionnsg = "BastionNICNSG"
+
+# Create the Bastion Host Standalone Virtual Machine
+# Create a virtual network card and associate with public IP address and NSG
+az network nic create --resource-group $group --name BastionNIC --subnet BaseSubnet --location $location \
+            --public-ip-address $bastionip --network-security-group $bastionnsg --vnet-name $vnet
+
+# Create a virtual machine configuration
+vmName = "vm-bastion-sc"
+az compute vm --resource-group $group --name $vmName
