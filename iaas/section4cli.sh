d3a784d01106cf829655356924adb21214026301
Brian C Harrison <briancharrison@Brians-MBP.fios-router.home>
2017-11-15 22:49:00 -0500

    Updates Section 4

--- iaas/section4cli.sh
+++ iaas/section4cli.sh
@@ -12,4 +12,20 @@ az network nic create --resource-group $group --name BastionNIC --subnet BaseSub
 
 # Create a virtual machine configuration
 vmName = "vm-bastion-sc"
-az compute vm --resource-group $group --name $vmName
+az compute vm --resource-group $group --name $vmName --admin-username brianadmin --location $location \
+            --size Standard_D1_V2 --authentication-type password --admin-password B0bb@F3tt0911 --nics BastionNIC \
+            --image UbuntuLTS
+
+# Create the DB Servers and add them to a newly created Availability Set and the Internal LB Backend Pool
+dblb = "PacktDBLB"
+dblbbepool = "DBBackendPool"
+dbnsg = "DBNSG"
+
+az vm availability-set create --resource-group $group --location $location --name DBAvailabilitySet \
+            --platform-update-domain-count 2 --platform-fault-domain-count 2 
+
+# Create the two NICs for the DB Servers and add to the Internal LB Backend Pool
+az network nic create --resource-group $group --name DBNIC1 --subnet DBSubnet --location $location \
+            --vnet-name $vnet --lb-name $dblb --lb-address-pools $dblbbepool
+az network nic create --resource-group $group --name DBNIC2 --subnet DBSubnet --location $location \
+            --vnet-name $vnet --lb-name $dblb --lb-address-pools $dblbbepool
